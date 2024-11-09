54c0b53d539f40953e36b80d018a35a40f1d8e07
adarsh-jaiss <its.adarshjaiss@gmail.com>
2024-11-10 00:03:00 +0530

    releasing v.1.0.0
    Signed-off-by: adarsh-jaiss <its.adarshjaiss@gmail.com>

---
+++ main.go
@@ -0,0 +1,4 @@
+// Package mailify provides functionalities for handling and processing emails.
+// This package includes features for sending, receiving, and managing email
+// communications in a secure and efficient manner.
+package mailify
